57db88e7aba9de1dde0ee293d6c06f31de5d1670
johnf <johnf@topsteptrader.com>
2020-08-01 20:57:46 -0500

    create UsersList

---
+++ src/components/users-list/UsersList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function UsersList() {
+  return (
+    <div className="UsersList">
+      <ul>
+          <li>John</li>
+          <li>Max</li>
+          <li>Pul</li>
+          <li>Khalil</li>
+      </ul>
+    </div>
+  );
+}
