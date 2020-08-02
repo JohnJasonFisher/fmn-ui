1a320c0464acdf13ed81090dc6a204d4905f2c05
John Fisher <jjfisher91@gmail.com>
2020-08-02 12:37:41 -0500

    add1

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>I am adding text</h1>
         Friday Night Magic!
       </header>
       <body>
